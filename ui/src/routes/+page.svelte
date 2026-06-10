1204aa35881bb95c2458fd63971a9d11550af776
Yikuan Sun <38194363+yikuansun@users.noreply.github.com>
2026-06-10 11:36:03 +0800

    Make table

--- ui/src/routes/+page.svelte
+++ ui/src/routes/+page.svelte
@@ -1,2 +1,41 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+    import { renderToString as renderLatex } from 'katex';
+    import "katex/dist/katex.min.css"
+
+    let T: number = 10;
+</script>
+
+{#snippet latex(s: string)}
+    {@html renderLatex(s)}
+{/snippet}
+
+<table>
+  <thead>
+    <tr>
+      <th>Period ({@render latex("t")})</th>
+      <th>Matriculated students ({@render latex("\\mathbf{m}_t")})</th>
+      <th>Waitlist ({@render latex("\\mathbf{n}_t")})</th>
+    </tr>
+  </thead>
+  <tbody>
+    {#each Array(T) as _, t}
+      <tr>
+        <td>{t}</td>
+        <td>{t}</td>
+        <td>{t}</td>
+      </tr>
+    {/each}
+  </tbody>
+</table>
+
+<style>
+    table {
+        border-collapse: collapse;
+    }
+
+    td, th {
+        border: 1px solid #dddddd;
+        text-align: left;
+        padding: 8px;
+    }
+</style>
