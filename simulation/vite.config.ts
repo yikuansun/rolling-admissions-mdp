c30aa6294cf143e6ee32c750545a4d4a03c163c5
Yikuan Sun <38194363+yikuansun@users.noreply.github.com>
2026-06-11 13:54:18 +0800

    Add svelte static adapter

--- simulation/vite.config.ts
+++ simulation/vite.config.ts
@@ -1,5 +1,5 @@
 import tailwindcss from '@tailwindcss/vite';
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
